51866fa111b79aea91a1b04aff536c9fc6d22db0
Uh-huh-Philip <philipjf@live.cn>
2013-08-27 20:21:50 +0800

    负责与数据库建立连接，判断数据库可用性

---
+++ Philip/LCloudServer/src/org/web/utility/DbUtility.java
@@ -0,0 +1,44 @@
+package org.web.utility;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbUtility {
+	
+	private Connection connection;
+	
+	public DbUtility(){
+		
+	}
+	
+	public void connect() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			System.out.println("驱动加载成功");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			System.out.println("驱动加载失败");
+		}
+		try {
+			this.connection = DriverManager.getConnection("jdbc:mysql://localhost/testforgpsapplist", "root", "");
+			System.out.println("连接成功");
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println("连接失败");
+		}
+	}
+	
+	public Connection getConnection(){
+		return this.connection;
+	}
+	
+	public void closeConnection(){
+		try {
+			this.connection.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+}
